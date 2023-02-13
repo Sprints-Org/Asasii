26d65966a194ebaebe2864398dc9331e99d914ad
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2023-02-13 23:37:16 +0200

    make header responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 (() => {
     feather.replace()
 })();
+
+const btnNavEl = $(".btn-mobile-nav");
+const headerEl = $(".header");
+
+$(".btn-mobile-nav").on("click", function () {
+    $(".header").toggleClass("nav-open");
+});
