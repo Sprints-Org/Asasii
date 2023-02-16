e9609c96b0e77d67dc860652b6dd011ae0ebac9c
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2023-02-16 17:36:34 +0200

    SP-8 create product view

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-(() => {
-    feather.replace()
-})();
-
-const btnNavEl = $(".btn-mobile-nav");
-const headerEl = $(".header");
-
-$(".btn-mobile-nav").on("click", function () {
-    $(".header").toggleClass("nav-open");
-});
+(function($) {
+    feather.replace();
+    $(".btn-mobile-nav").on("click", function () {
+        $(".header").toggleClass("nav-open");
+    });
+    
+    [...$('.small-product-images img')].forEach(element => {
+        element.addEventListener("click", function () {
+            $('.large-img')[0].src = element.src;
+        });
+    });
+    
+    $('#decrease')[0].addEventListener("click", function(){
+        if ($('#number').val() > 1) {
+            $('#number').val(parseInt($('#number').val()) - 1);
+        }
+    });
+    
+    $('#increase')[0].addEventListener("click", function(){
+        $('#number').val(parseInt($('#number').val()) + 1);
+    });
+})(jQuery);
