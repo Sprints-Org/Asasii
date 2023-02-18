ecd0ca77126c1858603889e6d82ee110249b2c6f
Ahmed Rabea Smaha <35260162+ahmedrabeasmaha@users.noreply.github.com>
2023-02-19 01:11:11 +0200

    create like and make js better

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 (function($) {
     feather.replace();
+
     $(".btn-mobile-nav").on("click", function () {
         $(".header").toggleClass("nav-open");
     });
     
-    [...$('.product-images img')].forEach(element => {
-        element.addEventListener("click", function () {
-            $('.product-main-img img')[0].src = element.src;
-        });
+    $('.product-images').children().children().on("click", function () {
+        $(".main-img").attr('src', this.src);
     });
-    
-    $('#decrease')[0].addEventListener("click", function(){
+
+    $('#decrease').on("click", function(){
         if ($('#number').val() > 1) {
             $('#number').val(parseInt($('#number').val()) - 1);
         }
     });
     
-    $('#increase')[0].addEventListener("click", function(){
+    $('#increase').on("click", function(){
         $('#number').val(parseInt($('#number').val()) + 1);
+    });
+
+    $('.like').children().on("click", function(){
+        $('.like').children().toggleClass("fa-regular").toggleClass("fa-solid");
     });
 })(jQuery);
