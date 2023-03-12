5eee120639317f9a00539afbaa5301d80e66b748
BassantShaaban55 <bassantshaaban99@gmail.com>
2023-03-12 16:04:59 +0200

    details page

--- asasii/src/app/components/product-details/product-details.component.ts
+++ asasii/src/app/components/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Products } from 'src/app/interfaces/products';
 import { ProductService } from 'src/app/services/product.service';
 import { environment } from 'src/environment/environment';
+import { ActivatedRoute, Router } from '@angular/router';
 declare function JSfun ():void;
 
 @Component({
@@ -9,22 +10,36 @@ declare function JSfun ():void;
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    JSfun();
-  }
-
-products:Products[]=[];
-  product :Products={} as Products;
- getimage(){
-    return `${environment.imageUrl}products/${this.product.image}`
-  } 
-  constructor(private productService:ProductService) {}
+export class ProductDetailsComponent implements OnInit {
+ 
+  id:string=''
+  product: Products = {} as Products;
+  products: Products[] = [];
+ 
+  constructor(private productService:ProductService, private route: ActivatedRoute) { };
+ 
   
   ngOnInit() {
-    this. productService.getProducts().subscribe((data:any) => {
-      this.products = data;
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
     });
+    this.productService.getProductById(this.id).subscribe((data: any) => {
+      this.products=(data);
+      
+      console.log(data);
+      console.log(this.products);       
+     
+    });
+    this.productService.getProducts().subscribe((data: any) => {
+      this.product=(data);
+      
+      console.log(data);
+      // console.log(this.Product);
+     
+    });
+    
 }
+getimage(image:string){
+  return `${environment.imageUrl}products/${image}`}
 
 }
